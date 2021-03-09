22d7cb1b5fd3444ef9db6ec0eb0b99a653611692
galinamorozova <41506249+galinamorozova@users.noreply.github.com>
2021-03-09 15:41:00 +0300

    Updates

---
+++ precache-manifest.728dcfe110744951a8d2fd49196b43a4.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7dca0aaf86264a93d226a9831bc97a",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "fa246b6dafbfb6658084",
+    "url": "/react-project/static/css/main.3691e37c.chunk.css"
+  },
+  {
+    "revision": "7985f77377b4ba8aa032",
+    "url": "/react-project/static/js/2.79833ad2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-project/static/js/2.79833ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa246b6dafbfb6658084",
+    "url": "/react-project/static/js/main.9fffa9a6.chunk.js"
+  },
+  {
+    "revision": "17fd116c25eae2a75d48",
+    "url": "/react-project/static/js/runtime-main.a50d161c.js"
+  },
+  {
+    "revision": "a4c1346d3104d2ea68689d9cd88ba62a",
+    "url": "/react-project/static/media/bird.a4c1346d.jpg"
+  },
+  {
+    "revision": "a31b4c556734f2200ece48822bc500c6",
+    "url": "/react-project/static/media/boat.a31b4c55.jpg"
+  },
+  {
+    "revision": "20969f61bb6442662a12438084cbd7f8",
+    "url": "/react-project/static/media/bridge.20969f61.jpg"
+  },
+  {
+    "revision": "a7ed2f787640cb7040936f473d5615ff",
+    "url": "/react-project/static/media/flowers.a7ed2f78.jpg"
+  },
+  {
+    "revision": "a79cc58e95bfbb82f6305dfaf6605605",
+    "url": "/react-project/static/media/green.a79cc58e.jpg"
+  },
+  {
+    "revision": "5ef16d1ec6e127334ab07f7621ee8f09",
+    "url": "/react-project/static/media/lake.5ef16d1e.jpg"
+  },
+  {
+    "revision": "9022f06b471594f3caf05bfcdc3a985b",
+    "url": "/react-project/static/media/landscape.9022f06b.jpg"
+  },
+  {
+    "revision": "fcc6d2a6a0ea2d1db34c308d481dcd48",
+    "url": "/react-project/static/media/man.fcc6d2a6.jpg"
+  },
+  {
+    "revision": "52fc3a51f7229d90c44e8ebd9de1cfb7",
+    "url": "/react-project/static/media/man2.52fc3a51.jpg"
+  },
+  {
+    "revision": "3231e889b2b2f666850661e1ed4ceea3",
+    "url": "/react-project/static/media/mist.3231e889.jpg"
+  },
+  {
+    "revision": "85030d2074a81fad8db73e0fbc504f72",
+    "url": "/react-project/static/media/mountain.85030d20.jpg"
+  },
+  {
+    "revision": "9b4d134cb3e04a5ee6e3f23612ce6b7b",
+    "url": "/react-project/static/media/red.9b4d134c.jpg"
+  },
+  {
+    "revision": "e920043134e3a72b4bf24fcdb1cdea2a",
+    "url": "/react-project/static/media/river.e9200431.jpg"
+  },
+  {
+    "revision": "3236f4af58b39825fc17ed88b1687cf4",
+    "url": "/react-project/static/media/s120.3236f4af.jpg"
+  },
+  {
+    "revision": "f8e76bcadceb9c4320950a6f6871a93d",
+    "url": "/react-project/static/media/sun.f8e76bca.jpg"
+  },
+  {
+    "revision": "a6cb94a37e256e3d384d7ee09aa6ff56",
+    "url": "/react-project/static/media/toscana.a6cb94a3.jpg"
+  },
+  {
+    "revision": "32cc93320c908b812eff6d9af09a0798",
+    "url": "/react-project/static/media/valley.32cc9332.jpg"
+  },
+  {
+    "revision": "34076716ba3957e5396b5a3ab0c88e07",
+    "url": "/react-project/static/media/woman.34076716.png"
+  },
+  {
+    "revision": "fddbf05b48898c7ba04fa58397d5195d",
+    "url": "/react-project/static/media/woman2.fddbf05b.jpg"
+  }
+]);
